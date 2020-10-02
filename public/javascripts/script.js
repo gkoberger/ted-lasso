4e74b6d684d4bf87034b4cc199534723f2f736b6
gkoberger <gkoberger@gmail.com>
2020-10-01 22:58:32 -0700

    first commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,76 @@
-$.page('index', function() {
+$.page('index', function () {
+  $('.overflow').each(function () {
+    var i = Math.floor(Math.random() * 5);
+    $(this).addClass(`seat-${i}`);
+  });
 
+  $('.red').removeClass('red');
+
+  $('#go button').click(function() {
+    $('#go').remove();
+    setTimeout(function() {
+    $('audio')[0].play();
+    const check = function() {
+      var $s = $('.section:has(.is-red):not(.started)').eq(0);
+      if(!$s.length) return clearInterval(inv);
+      $s.addClass('started');
+      addRed($s);
+    };
+    var inv = setInterval(check, 3000);
+    check();
+    }, 1000);
+  });
+
+  function addRed($s) {
+    var start = $s.find('.is-red:not(.red)').last();
+
+    var left = 0;
+    var i = 0;
+    function next(start) {
+      if (!start.length || start.is('.ready') || !start.is('.is-red') || start.is('.red')) return false;
+      left++;
+      setTimeout(function() {
+        left--;
+        if (left === 0) {
+          if ($s.find('.is-red:not(.red)').length) {
+            addRed($s);
+          }
+        }
+      }, 300);
+
+      start.addClass('ready');
+      i++;
+      setTimeout(function () {
+        $(start).addClass('red');
+        var c = $(start).index();
+        var $c = $(start).parent().find('.chair');
+
+        var el_l = next($c.eq(c - 1));
+        var el_r = next($c.eq(c + 1));
+
+        var r = $(start).parent().index();
+        var el_t = next(
+          $('.row', $(start).closest('.section'))
+            .eq(r - 1)
+            .find('.chair')
+            .eq(c),
+        );
+        var el_b = next(
+          $('.row', $(start).closest('.section'))
+            .eq(r + 1)
+            .find('.chair')
+            .eq(c),
+        );
+
+        if (!el_t && !el_b && !el_l && !el_r) {
+          console.log("DONE");
+        }
+      }, 150 + Math.floor(Math.random() * 150));
+      return true;
+    }
+    next(start);
+  }
+
+  $('.section').eq(14).find('.is-red').eq(18).append($('<img>').attr('src', 'https://p195.p4.n0.cdn.getcloudapp.com/items/bLuwrBWw/image-removebg-preview%20(2).png?v=ae814620687836d2e121a9197e5a5f8c'));
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const async = require('async');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Home' });
+  var rows = new Array(16);
+  var cols = new Array(13);
+
+  var asciimo = require('asciimo').Figlet;
+
+  var text = req.query.text || 'TED!';
+  var out = [];
+
+  var on = (t, r, c) => {
+    t = t.split('\n');
+    r = r-1;
+    r = Math.floor(r / 2);
+    c;
+    try {
+    return t[r][c] === 'X' ? 'is-red' : '';
+    } catch(e) {
+      return '';
+    }
+  };
+
+  text = ` ${text} `.toUpperCase();
+  async.eachSeries(text.split(''), (l, n) => {
+    var called = false;
+    asciimo.write(l, 'alligator3', function(art){
+      if (called) return;
+      var a = art.replace(/[^\s]/g, 'X');
+      a = a.split('\n').map(e => ` ${e} `).join('\n');
+      called = true;
+      out.push(a);
+      n();
+    });
+  }, () => {
+    res.render('index', { title: 'Home', rows, cols, out, on, text: text.trim() });
+  });
 });
 
 module.exports = router;
