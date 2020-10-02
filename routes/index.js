ba8273a98ebe2e2b0b95ebec7a9994872e747776
gkoberger <gkoberger@gmail.com>
2020-10-01 23:40:23 -0700

    Make work on Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,80 @@
 const express = require('express');
 const router = express.Router();
 const async = require('async');
+const fs = require('fs');
+const _ = require('lodash');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   var rows = new Array(16);
   var cols = new Array(13);
 
-  var asciimo = require('asciimo').Figlet;
-
   var text = req.query.text || 'TED!';
   var out = [];
 
   var on = (t, r, c) => {
     t = t.split('\n');
-    r = r-1;
+    r = r - 1;
     r = Math.floor(r / 2);
     c;
     try {
-    return t[r][c] === 'X' ? 'is-red' : '';
-    } catch(e) {
+      return t[r][c] === 'X' ? 'is-red' : '';
+    } catch (e) {
       return '';
     }
   };
 
+  const font = require('../font.json');
+
   text = ` ${text} `.toUpperCase();
-  async.eachSeries(text.split(''), (l, n) => {
-    var called = false;
-    asciimo.write(l, 'alligator3', function(art){
-      if (called) return;
-      var a = art.replace(/[^\s]/g, 'X');
-      a = a.split('\n').map(e => ` ${e} `).join('\n');
-      called = true;
-      out.push(a);
-      n();
-    });
-  }, () => {
-    res.render('index', { title: 'Home', rows, cols, out, on, text: text.trim() });
+  _.each(text.split(''), function(t) {
+    out.push(font[t]);
   });
+
+  res.render('index', {
+    title: 'Home',
+    rows,
+    cols,
+    out,
+    on,
+    text: text.trim(),
+  });
+});
+
+router.get('/setup', (req, res, next) => {
+  var asciimo = require('asciimo').Figlet;
+  var out = {};
+  text = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ!-,.?:<>@#$%^&*(){}[]';
+  async.eachSeries(
+    text.split(''),
+    (l, n) => {
+      var called = false;
+      asciimo.write(l, 'alligator3', function (art) {
+        console.log(art);
+        if (called) return;
+        var a = art.replace(/[^\s]/g, 'X');
+        a = a
+          .split('\n')
+          .map(e => ` ${e} `)
+          .join('\n');
+        called = true;
+
+        out[l] = a;
+        n();
+      });
+    },
+    () => {
+      fs.writeFileSync(`./font.json`, JSON.stringify(out, undefined, 2));
+      res.render('index', {
+        title: 'Home',
+        rows,
+        cols,
+        out,
+        on,
+        text: text.trim(),
+      });
+    },
+  );
 });
 
 module.exports = router;
