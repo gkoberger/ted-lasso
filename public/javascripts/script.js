ae04a53ff9a202c763f9c55bbc215d334721d566
gkoberger <gkoberger@gmail.com>
2020-10-01 23:24:34 -0700

    Fix up a bit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,19 +6,25 @@ $.page('index', function () {
 
   $('.red').removeClass('red');
 
-  $('#go button').click(function() {
-    $('#go').remove();
-    setTimeout(function() {
-    $('audio')[0].play();
-    const check = function() {
-      var $s = $('.section:has(.is-red):not(.started)').eq(0);
-      if(!$s.length) return clearInterval(inv);
-      $s.addClass('started');
-      addRed($s);
-    };
-    var inv = setInterval(check, 3000);
-    check();
-    }, 1000);
+  $('#go button').click(function () {
+    $('#body-parent').load('/?text=' + $('#text').val() + ' #body', () => {
+      $('.overflow').each(function () {
+        var i = Math.floor(Math.random() * 5);
+        $(this).addClass(`seat-${i}`);
+      });
+      $('#go').remove();
+      setTimeout(function () {
+        $('audio')[0].play();
+        const check = function () {
+          var $s = $('.section:has(.is-red):not(.started)').eq(0);
+          if (!$s.length) return clearInterval(inv);
+          $s.addClass('started');
+          addRed($s);
+        };
+        var inv = setInterval(check, 3000);
+        check();
+      }, 1000);
+    });
   });
 
   function addRed($s) {
@@ -27,9 +33,15 @@ $.page('index', function () {
     var left = 0;
     var i = 0;
     function next(start) {
-      if (!start.length || start.is('.ready') || !start.is('.is-red') || start.is('.red')) return false;
+      if (
+        !start.length ||
+        start.is('.ready') ||
+        !start.is('.is-red') ||
+        start.is('.red')
+      )
+        return false;
       left++;
-      setTimeout(function() {
+      setTimeout(function () {
         left--;
         if (left === 0) {
           if ($s.find('.is-red:not(.red)').length) {
@@ -63,7 +75,7 @@ $.page('index', function () {
         );
 
         if (!el_t && !el_b && !el_l && !el_r) {
-          console.log("DONE");
+          console.log('DONE');
         }
       }, 150 + Math.floor(Math.random() * 150));
       return true;
@@ -71,6 +83,14 @@ $.page('index', function () {
     next(start);
   }
 
-  $('.section').eq(14).find('.is-red').eq(18).append($('<img>').attr('src', 'https://p195.p4.n0.cdn.getcloudapp.com/items/bLuwrBWw/image-removebg-preview%20(2).png?v=ae814620687836d2e121a9197e5a5f8c'));
+  $('.section')
+    .eq(14)
+    .find('.is-red')
+    .eq(18)
+    .append(
+      $('<img>').attr(
+        'src',
+        'https://p195.p4.n0.cdn.getcloudapp.com/items/bLuwrBWw/image-removebg-preview%20(2).png?v=ae814620687836d2e121a9197e5a5f8c',
+      ),
+    );
 });
-
